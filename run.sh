ec8ba7821c20ff680b629e840291b24da1f768f4
termuxparadevs <termuxparadevs@outlook.com>
2023-01-22 04:16:50 -0300

    modifica mensagem de ja instalado

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------------------------
 # Iniciando 
 echo -e "\e[35;1mTMD iniicianco customização...\e[0m"
-echo ""
+echo " "
 
 # Este script verifica se o git está instalado no sistema. Caso não esteja, 
 # ele faz a instalação do git usando o comando pkg install git -y. 
@@ -18,7 +18,7 @@ if ! [ -x "$(command -v git)" ]; then
   echo 'Installing git...'
   pkg install git -y
 else
-  echo -e "git >> \e[32;1mok\033[0m"
+  echo -e " git >> \e[32;1mok\033[0m"
 fi
 
 # Esse script verifica que o zsh já está instalado, caso contrário
@@ -29,7 +29,7 @@ if ! [ -x "$(command -v zsh)" ]; then
   echo 'Installing zsh...'
   pkg install zsh -y
 else
-  echo -e "zsh >> \e[32;1mok\033[0m"
+  echo -e " zsh >> \e[32;1mok\033[0m"
 fi
 
 # Esse script verifica se o oh-my-zsh está inslado
@@ -39,21 +39,22 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installing oh-my-zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
-  echo -e "oh-my-zsh >> \e[32;1mok\033[0m"
+  echo -e " oh-my-zsh >> \e[32;1mok\033[0m"
 fi
 
 
 # Realizando o procedimento de clone e instalação\configuração dos recursos
 if [ ! -d "$HOME/zsh-syntax-highlighting" ]; then
-  echo -e "zsh syntaxe hig... \e[35;1m não está instalado!\031[0m"
+  echo -e "zsh syntaxe hig... \e[31;1m não está instalado!\031[0m"
   echo -e "Realizando \e[35;1minstalação...!\032[0m"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
   echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
   echo "zsh-syntax-highlighting installed."
 else
-  echo -e "zsh-syntaxe-high... \e[i32;1mok\033[0m"
+  echo -e " zsh-syntaxe-high... \e[i32;1mok\033[0m"
 fi
 
+# Verifica se tem alterações no arquivo .zshrc 
 if ! grep -q "zsh-syntax-highlighting" $HOME/.zshrc; then
   echo "Adding zsh-syntax-highlighting to .zshrc"
   echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
