de4f06d5a65e652c6e8a1993e0d0e736dccefde3
termuxparadevs <termuxparadevs@outlook.com>
2023-01-22 15:32:33 -0300

    corrige instalacao do zsh sintaxe highlight

--- run.sh
+++ run.sh
@@ -44,7 +44,7 @@ fi
 
 
 # Realizando o procedimento de clone e instalação\configuração dos recursos
-
+rm -rf ~/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
